80b2f81c394f53fa172b8f511db87f59d31e8cd4
meadonw <babbler234@gmail.com>
2025-10-22 00:25:21 +0300

    Update 1.py

--- second/1.py
+++ second/1.py
@@ -1,51 +1,44 @@
-from math import *
+import math
 
 
 def solve_circle(radius):
-    perimeter = 2 * pi * radius
-    area = pi * radius ** 2
+    perimeter = 2 * math.pi * radius
+    area = math.pi * radius ** 2
     print("Периметр круга:", perimeter)
     print("Площадь круга:", area)
 
 
 def find_perimeter(coordinates):
-    if coordinates:
-        p = 0
-        n = len(coordinates)
-        for i in range(n):
-            p += distance(coordinates[i], coordinates[(i + 1) % n])
-        return p
-    else:
-        return 0
-
-
-def find_polygon_area(coordinates):
-    area = 0
     n = len(coordinates)
+    perimeter = 0
+    for i in range(n):
+        x1, y1 = coordinates[i]
+        x2, y2 = coordinates[(i + 1) % n]
+        perimeter += math.hypot(x2 - x1, y2 - y1)
+
+    return perimeter
+
+
+def find_area(coordinates):
+    n = len(coordinates)
+    area = 0
     for i in range(n):
         x1, y1 = coordinates[i]
         x2, y2 = coordinates[(i + 1) % n]
         area += x1 * y2 - x2 * y1
+
     return abs(area) / 2
-
-
-def distance(coordinate1, coordinate2):
-    return ((coordinate1[0] - coordinate2[0]) ** 2 + (coordinate1[1] - coordinate2[1]) ** 2) ** 0.5
 
 
 def solver(coordinates):
     if len(coordinates) == 3:
-        print("Площадь треугольника:", find_triangle_area(*coordinates))
-        print("Периметр треугольника:", round(find_perimeter(coordinates), 2))
+        print("Площадь треугольника:", find_area(coordinates))
+        print("Периметр треугольника:", find_perimeter(coordinates))
     elif len(coordinates) >= 4:
-        print("Площадь многоугольника:", find_polygon_area(coordinates))
+        print("Площадь многоугольника:", find_area(coordinates))
         print("Периметр многоугольника:", find_perimeter(coordinates))
     else:
-        print("Введите больше координат вершин для определения фигуры")
-
-
-def find_triangle_area(p1, p2, p3):
-    return 0.5 * abs((p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0]))
+        print("Многоугольник должен иметь как минимум 3 вершины")
 
 
 def main():
